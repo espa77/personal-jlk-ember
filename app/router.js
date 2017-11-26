43ebd552a1d1ea82dff60d5dedcf4471f483dc4d
espa77 <ethansteiner@gmail.com>
2017-11-26 14:46:13 -0500

    Added navigation and slider for testimonials

--- app/router.js
+++ app/router.js
@@ -1,12 +1,16 @@
-import Ember from 'ember';
+import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
 
-const Router = Ember.Router.extend({
+const Router = EmberRouter.extend({
   location: config.locationType,
   rootURL: config.rootURL
 });
 
 Router.map(function() {
+  this.route('conditions-treated');
+  this.route('new-patients');
+  this.route('about');
+  this.route('treatment-philosophy');
 });
 
 export default Router;
